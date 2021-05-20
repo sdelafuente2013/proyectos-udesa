4234c3944ae57f8aee78df93f14e8e4922716ecc
sdelafuente2013 <santiagodelafuente2013@gmail.com>
2021-05-20 11:08:37 -0300

    Ejercicio 1 - resuelto

---
+++ Introducción, variables y tipos de datos/ejercicio1.js
@@ -0,0 +1,7 @@
+//creamos las variables, recomendamos siempre que puedan crearla con const y no let (salvo que la vayan a modificar posteriormente)
+const edad = 24
+const peso = 71.5
+
+//imprimimos las dos variables y se nos muestra por consola
+console.log(edad)
+console.log(peso)
