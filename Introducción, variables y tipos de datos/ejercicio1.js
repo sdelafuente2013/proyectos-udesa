f72ac9c175f0fbbeef34cd2c78691037b67f4a52
sdelafuente2013 <santiagodelafuente2013@gmail.com>
2021-05-20 11:11:42 -0300

    Ejercicio 2 - resuelto

--- Introducción, variables y tipos de datos/ejercicio1.js
+++ Introducción, variables y tipos de datos/ejercicio1.js
@@ -1,6 +1,6 @@
 //creamos las variables, recomendamos siempre que puedan crearla con const y no let (salvo que la vayan a modificar posteriormente)
-const edad = 24
-const peso = 71.5
+const edad;
+const peso;
 
 //imprimimos las dos variables y se nos muestra por consola
 console.log(edad)
---
+++ Introducción, variables y tipos de datos/ejercicio2.js
@@ -0,0 +1,5 @@
+let meses = 12;
+let cantidadDeAlumnos = 30;
+
+console.log(meses);
+console.log(cantidadDeAlumnos);
