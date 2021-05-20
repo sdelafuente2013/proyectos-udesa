5ce931155b4c0de418193b2571ae025ed71e385f
sdelafuente2013 <santiagodelafuente2013@gmail.com>
2021-05-20 11:37:53 -0300

    Ejercicio 5 - resuelto

---
+++ Introducción, variables y tipos de datos/ejercicio5.js
@@ -0,0 +1,18 @@
+//Algo interesante como llamamos a las variables con un estilo Camel case (estilizado como camelCase) o letra de caja camello es un estilo de escritura que se aplica a frases o palabras compuestas, es decir, para dar un espacio ponemos una mayuscula, por ejemplo: nombreEstudiante, notaFinal, totalTrimestre, etc..
+
+let meGustaElHelado = true;
+
+console.log(meGustaElHelado);
+
+let sumaTotal = 5 + 10; // resultado: 15
+let restaTotal = 20 - 5; // resultado: 15 
+let multiplicar = sumaTotal * restaTotal;// resultado: 225
+let operacionBoolean = 2 > 1; // resultado: true
+let operacionBoolean2 = 1 > 1; // resultado: false   
+
+
+console.log(`La suma total es: ${sumaTotal}`);
+console.log(`La resta total es: ${restaTotal}`);
+console.log(`La multiplicacion entre la variable "sumaTotal" y "restaTotal" es: ${multiplicar}`);
+console.log(`La "operacionBoolean" da como resultado: ${operacionBoolean}`);
+console.log(`La "operacionBoolean2" da como resultado: ${operacionBoolean2}`);
