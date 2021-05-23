a4661aafdb1027f77b7d33dae1fe274e27b4501d
sdelafuente2013 <santiagodelafuente2013@gmail.com>
2021-05-23 18:13:13 -0300

    Ejercicios 4 y 5 resueltos (Funciones)

--- Funciones, Arrays y objetos literales/ejercicioFunciones.js
+++ Funciones, Arrays y objetos literales/ejercicioFunciones.js
@@ -1,23 +1,43 @@
-// 1)
+// 1) Funcion para calcular el area de un cuadrado
 
 function calcularAreaCuadrado(altura, ancho) {
     return `El area del cuadrado es: ${altura * ancho}`;
 };
 
-// 2)
+// 2) Funcion para calcular el area de un circulo
 const PI = 3.14;
 
 function calcularAreaCirculo(radio, PI) {
     return `El area del circulo es: ${PI * radio ** 2}`;
 }
 
-// 3)
-const listaNumeros = [1,2,3,4,5]
+// 3) Funcion que recibe un array de numeros y devuelve el promedio
+const listaNumeros = [1, 2, 3, 4, 5]
+
 function promedioCincoNumeros(listaNumeros) {
     let promedio = 0;
     for (let index = 0; index < 5; index++) {
-        promedio += listaNumeros[index];          
+        promedio += listaNumeros[index];
     }
     promedio /= 5;
-    return `El promedio de la lista de 5 numeros es: ${promedio}`;
+    return `El promedio de la lista de numeros es: ${promedio}`;
 }
+
+// 4) Funcion que recibe un array y retorna la cantidad de elementos que contiene
+
+const diasSemanales = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+function cantidadElementos(diasSemanales) {
+    return diasSemanales.length;
+};
+
+// 5) Funcion que recibe un array y retorna la posicion "Miercoles" del array diasSemanales
+const diasSemanales2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+function buscarMiercoles(diasSemanales) {
+    for (let index = 0; index < diasSemanales.length; index++) {
+        if (diasSemanales[index] === "Miercoles") {
+            return index;
+        };
+    };
+};
