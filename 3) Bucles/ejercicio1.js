eddf17bb5116561722f337a4be99f2eb3e02c70f
sdelafuente2013 <santiagodelafuente2013@gmail.com>
2021-05-25 21:09:42 -0300

    Ejercicio 1 resuelto (Bucles)

---
+++ 3) Bucles/ejercicio1.js
@@ -0,0 +1,26 @@
+// 1) 
+
+//A-
+for (let index = 0; index < 5; index++) {
+    console.log("Practicando con el bucle for");
+};
+
+//B-
+for (let i = 0; i < 10; i++) {
+    console.log(`La variable ${i} tiene el valor ${i}`);
+};
+
+//C- i
+for (let index = 0; index < 20; index += 2) {
+    console.log(`Multiplo ${index}° de 2 es: ${index}`);
+};
+
+//C- ii
+for (let index = 0; index < 105; index += 5) {
+    console.log(`Multiplo ${index}° de 5 es: ${index}`);
+};
+
+//D
+for (let index = 100; index >= 0; index--) {
+    console.log(index)
+}
