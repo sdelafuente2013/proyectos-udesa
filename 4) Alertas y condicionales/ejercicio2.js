9caa649be89800bc188bafedc2ad2a405d15b2fb
sdelafuente2013 <santiagodelafuente2013@gmail.com>
2021-05-27 00:23:29 -0300

    Ejercicio 2 resuelto (condicionales)

--- 4) Alertas y condicionales/ejercicio2.js
+++ 4) Alertas y condicionales/ejercicio2.js
@@ -1,9 +1,16 @@
+// 2)
+
 let numeroElegido = Number(prompt("Ingrese un numero del 1 al 100: "));
 let numeroElegido2 = Number(prompt("Ingrese otro numero del 1 al 100: "));
 let resultado = numeroElegido + numeroElegido2;
 
-if(numeroElegido !== numeroElegido2){
-    console.log(`El resultado de la suma es: ${resultado}`);
-}else if(numeroElegido === "" || numeroElegido2 === "" || numeroElegido === numeroElegido2){
-    console.log("Los numeros no pueden ser iguales o no pueden estar vacios")
+if (numeroElegido !== numeroElegido2 && numeroElegido !== "" && numeroElegido2 !== "") {
+    if (resultado < 10) {
+        console.log(`La suma de tus números es ${resultado} y es menor a 10 `);
+    } else if (resultado > 10 && resultado < 20) {
+        console.log("Bien pasaste");
+    } else {
+        console.log("Te zarpaste");
+    };
+
 }
